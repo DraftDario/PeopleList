3d29616e9f6adf62f8307132ba3ad35d9944a753
DraftDario <dario.mosca@he-arc.ch>
2016-11-02 21:56:08 +0100

    DeletePerson
    Ajout de la suppression des personnes et ajout de la séparation des
    actions par page xhtml

--- src/java/ch/hearc/ig/odi/peoplelist/service/Services.java
+++ src/java/ch/hearc/ig/odi/peoplelist/service/Services.java
@@ -21,21 +21,21 @@ import javax.inject.Named;
  */
 @SessionScoped
 @Stateful
-@Named(value="services")
-public class Services implements Serializable{
-    
+@Named(value = "services")
+public class Services implements Serializable {
+
     // Permet de stocker la liste de personnes de telle manière à simuler une base de données
     private Map<Long, Person> people;
-    
+
     // Compteur permettant d'avoir des IDs uniques lors de l'ajout de nouvelles personnes
     private Long currentMaxId = 1l;
-    
-    public Services(){
+
+    public Services() {
         // Crée 6 personnes à chaque création de session pour avoir une base sur laquelle travailler
         try {
             people = new HashMap<>();
             SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-            
+
             Person currentPerson = new Person();
             currentPerson.setId(1l);
             currentPerson.setGender("male");
@@ -44,7 +44,7 @@ public class Services implements Serializable{
             currentPerson.setBirthDate(sdf.parse("22.01.1972"));
             currentPerson.setMarried(false);
             people.put(1l, currentPerson);
-            
+
             currentPerson = new Person();
             currentPerson.setId(2l);
             currentPerson.setGender("male");
@@ -53,7 +53,7 @@ public class Services implements Serializable{
             currentPerson.setBirthDate(sdf.parse("27.08.1981"));
             currentPerson.setMarried(true);
             people.put(2l, currentPerson);
-            
+
             currentPerson = new Person();
             currentPerson.setId(3l);
             currentPerson.setGender("male");
@@ -62,7 +62,7 @@ public class Services implements Serializable{
             currentPerson.setBirthDate(sdf.parse("12.06.1970"));
             currentPerson.setMarried(false);
             people.put(3l, currentPerson);
-            
+
             currentPerson = new Person();
             currentPerson.setId(4l);
             currentPerson.setGender("female");
@@ -71,7 +71,7 @@ public class Services implements Serializable{
             currentPerson.setBirthDate(sdf.parse("04.08.1981"));
             currentPerson.setMarried(true);
             people.put(4l, currentPerson);
-            
+
             currentPerson = new Person();
             currentPerson.setId(5l);
             currentPerson.setGender("female");
@@ -80,7 +80,7 @@ public class Services implements Serializable{
             currentPerson.setBirthDate(sdf.parse("06.12.1972"));
             currentPerson.setMarried(false);
             people.put(5l, currentPerson);
-            
+
             currentPerson = new Person();
             currentPerson.setId(6l);
             currentPerson.setGender("female");
@@ -89,15 +89,17 @@ public class Services implements Serializable{
             currentPerson.setBirthDate(sdf.parse("25.04.1969"));
             currentPerson.setMarried(false);
             people.put(6l, currentPerson);
-            
+
             currentMaxId = 6l;
         } catch (ParseException ex) {
             Logger.getLogger(Services.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     /**
-     * Sauvegarde une nouvelle personne dans la liste, et retourne l'id de ladite nouvelle personne
+     * Sauvegarde une nouvelle personne dans la liste, et retourne l'id de
+     * ladite nouvelle personne
+     *
      * @param gender Genre de la personne
      * @param firstName Prénom de la personne
      * @param lastName Nom de la personne
@@ -105,33 +107,37 @@ public class Services implements Serializable{
      * @param birthDate Date de naissance de la personne
      * @return L'ID de la nouvelle personne
      */
-    public Long savePerson(String gender, String firstName, String lastName, Boolean married, Date birthDate){
+    public Long savePerson(String gender, String firstName, String lastName, Boolean married, Date birthDate) {
         Person person = new Person();
-        
+
         currentMaxId++;
-        
+
         person.setId(currentMaxId);
         person.setGender(gender);
         person.setFirstName(firstName);
         person.setLastName(lastName);
         person.setBirthDate(birthDate);
         person.setMarried(married);
-        
+
         people.put(currentMaxId, person);
-        
+
         return currentMaxId;
     }
-    
-    public Person getPerson(Long id){
+
+    public Person getPerson(Long id) {
         return people.get(id);
     }
-    
-    public Map<Long, Person> getPeople(){
+
+    public Map<Long, Person> getPeople() {
         return people;
     }
-    
-    public List<Person> getPeopleList(){
+
+    public List<Person> getPeopleList() {
         return new ArrayList(people.values());
     }
-    
+
+    public void deletePerson(Person person) {
+        people.remove(person.getId());
+    }
+
 }
